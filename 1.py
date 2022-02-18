b903639bc6adb886c7ddfdf24a1d1d67276dc772
djj45 <ssjj_djj45@163.com>
2022-02-18 14:52:34 +0800

    改进srt文件命名

--- 1.py
+++
@@ -1,6 +0,0 @@
-PATH = r"C:\Users\23027\AppData\Local\JianyingPro\User Data\Projects\com.lveditor.draft\202202011537\draft_content.json"
-
-with open(PATH, "r", encoding="utf-8") as f:
-    line = f.readline()
-    print(type(line))
-    f.close()
